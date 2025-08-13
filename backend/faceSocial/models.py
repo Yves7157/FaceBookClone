592da1b9fdc75799b540abec20fe298c88ffc064
Yves Mulamba <yvesmulamba641@gmail.com>
2025-08-12 22:03:16 -0500

    first commit

---
+++ backend/faceSocial/models.py
@@ -0,0 +1,13 @@
+# from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+
+# class UserProfile(AbstractUser):
+#     profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True, verbose_name="Profile Picture")
+#     bio = models.TextField(max_length=500, null=True, blank=True, verbose_name="Bio")
+#     location = models.CharField(max_length=100, null=True, blank=True, verbose_name="Location")
+#     birth_date = models.DateField(null=True, blank=True, verbose_name="Birth Date")
+#     website = models.URLField(max_length=200, null=True, blank=True, verbose_name="Website")
+#     phone_number = models.CharField(max_length=15, null=True, blank=True, verbose_name="Phone Number")
+#     is_verified = models.BooleanField(default=False)
+
