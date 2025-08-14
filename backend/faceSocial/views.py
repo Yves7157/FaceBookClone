3c39861b4be9125761a5b499de538808f7198f16
Yves Mulamba <yvesmulamba641@gmail.com>
2025-08-13 22:22:33 -0500

    I have fixed navbar and added adjustment to it

--- backend/faceSocial/views.py
+++ backend/faceSocial/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'faceSocial/index.html')
+
+def messages(request):
+    return render(request, 'faceSocial/messages.html')
